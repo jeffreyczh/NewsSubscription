3f70fc0f506b9cbda2c11a2a920acce61cabe33b
jeffrey chen <jeffreychen2013@gmail.com>
2013-12-31 14:04:19 -0800

    test GET

--- server.js
+++ server.js
@@ -2,5 +2,18 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.write('ready...\n');
+  getPage('http://csb.stanford.edu/class/public/pages/sykes_webdesign/05_simple.html', res);
+  
 }).listen(port);
+
+function getPage(url, res) {
+	http.get(url, function(subres) {
+		subres.on('end', function (chunk) {
+			console.log('BODY: ' + chunk);
+		});
+	}).on('error', function(e) {
+		res.write("Got error: " + e.message);
+	});
+	res.end();
+}
